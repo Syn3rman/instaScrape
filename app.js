42c2e3718a7b16d9213be0547c7d0b34e4df2d61
KJSCE <aditya10699@gmail.com>
2019-08-06 15:03:39 +0000

    added express server

--- app.js
+++ app.js
@@ -1,80 +1,15 @@
-const puppeteer = require('puppeteer');
-const creds = require('./creds');
-const async = require('async');
-const request = require('request');
+const ig = require('./app');
+const express = require('express');
+
+var app = express();
+const port = 8000;
+app.listen(port, ()=>{
+	console.log("Listening on port 8000");
+});
+
+app.set('trust proxy', true);
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
 
-const url = 'https://www.instagram.com/accounts/login/?source=auth_switcher'
-const profile = (handle)=>{
-	return `https://www.instagram.com/${handle}/`
-}
-
-
-let instagram = {
-	browser: null,
-	page: null,
-
-	initialize: async ()=>{
-		instagram.browser = await puppeteer.launch({headless: false});
-		instagram.page = await instagram.browser.newPage();
-	},
-
-	login: async ()=>{
-		await instagram.page.goto(url, {waitUntil: 'networkidle2'});
-		await instagram.page.waitFor("input[name='username']");
-
-		// Login to the account
-		await instagram.page.type("input[name='username']", creds.username, {delay: 100});
-		await instagram.page.type("input[name='password']", creds.password, {delay: 100});
-		await instagram.page.click("button[type='submit']");	
-	},
-	scrapeProfile: async (handle)=>{
-		profilePage = profile(handle);	
-		console.log(profilePage);
-		result = []
-		count = 0 
-		// Implement feature to stop after either getting 100 posts or if no new element is selectded for say 10 iterations
-		// len_over_time = []
-		await instagram.page.goto(profilePage, {waitUntil: 'networkidle2'});
-		await instagram.page.waitFor('a>div>div.KL4Bh');
-		while(count<100){
-			console.log(result.length, count);
-			const nodes = await instagram.page.$$("a>div>div.KL4Bh");
-			console.log('Selected nodes: ', nodes.length);
-			// console.log(Object.keys(nodes[0]));
-			for(let i=0;i<nodes.length;i++){
-				// console.log(typeof nodes[i]);
-				res = await instagram.page.evaluate(el => el.firstChild.src, nodes[i]);
-				if(!result.includes(res)){
-					result.push(res);
-					count+=1;
-				} 
-			}
-			// console.log(result);
-			await instagram.page.evaluate(_ => {
-				window.scrollBy(0, window.innerHeight);
-			  });
-			await instagram.page.waitFor(3000);
-			}
-		instagram.download(result, handle);	
-	},
-	download: async (urls, handle)=>{
-		await request({
-				uri: "http://localhost:5000/downloadImages",
-				method: "POST",
-				body: {
-					nprocs: 8,
-					urls: urls,
-					handle: handle
-				},
-				json: true,
-				}, function(error, response, body) {
-				console.log(body);
-			});
-	},
-	close: async ()=>{
-		await instagram.browser.close();
-	},
-}
-
-module.exports = instagram;
+app.use('/', require('./server/'));
